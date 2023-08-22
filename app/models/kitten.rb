bd11c5271149db1a48917d10f86b61a496020699
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-22 14:28:26 +0900

    add validations to kitten model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,9 @@
 class Kitten < ApplicationRecord
+  validates :name, :age, :cuteness, :softness, presence: true
+  validates :name, length: { in: 3..32 }
+  validates :age, :cuteness, :softness, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 100
+  }
 end
